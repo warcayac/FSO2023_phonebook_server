b8699e982085f078b8c48e777863c30bf9f512c4
William Arcaya C. <warcayac@live.com>
2024-01-19 15:41:08 -0500

    Static pages added

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import Elysia from "elysia";
 import cors from "@elysiajs/cors";
+import staticPlugin from "@elysiajs/static";
 
 import personsRoutes from "./plugins/paths/persons/personsRoutes";
 import { getInfo } from "./handlers";
@@ -10,6 +11,7 @@ import wlogger  from './plugins/wac-logger/index';
 const app = new Elysia()
   .use(cors({methods: '*'}))  
   .use(wlogger(true))
+  .use(staticPlugin({ assets: 'dist', prefix: '/' }))
   .group('/api', app => app.use(personsRoutes))
   .get('/info', getInfo)
   .all('*', () => httpResponse[404]('Path name not found'))
